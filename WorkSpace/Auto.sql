07d53005966cce6504c37c62e35526f36f491db9
joshhcamargo <167809583+joshhcamargo@users.noreply.github.com>
2024-05-20 16:56:07 -0300

    Add files via upload

---
+++ WorkSpace/Auto.sql
@@ -0,0 +1,19 @@
+ #Comando para criar a base de dados
+ create database auto;
+ use auto;
+ 
+ #Selecione a base de dados
+ use auto;
+ 
+ #Criar tabela
+ create table carro(
+       id int auto_increment not null,
+       marca varchar(60) null,
+       ano int null,
+       primary key(id)
+ );
+ 
+ #Selecionar tabela
+ select  * from carro;
+ 
+ 
