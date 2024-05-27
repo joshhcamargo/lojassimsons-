1c41558512c7d3d8e5571029856492b3c695126e
joshhcamargo <167809583+joshhcamargo@users.noreply.github.com>
2024-05-27 16:57:10 -0300

    Add files via upload

---
+++ bd.sql
@@ -0,0 +1,11 @@
+Create database auto;
+use auto;
+
+ Create table carro(
+    id int auto_increment not null,
+    marca varchar (120) null,
+    ano varchar (120) null,
+    primary key(id)
+ )
+ use auto;
+ select * from carro;
